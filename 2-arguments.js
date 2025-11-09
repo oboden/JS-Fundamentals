f0e302db0c6ef783f1e83554c6ae074fd1b91b64
michael <michaelesigbone@gmail.com>
2025-11-09 15:50:41 +0100

    New Addition

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
-const newFunc = function (args) {
- if(arguments.length === 0) {
-  console.log('No argument')
- } else if (arguments.length === 1); {
-  console.log('Argument found');
- } else {
-  console.log('Arguments found')
- }
-}
+const newFunc = function () {
+  if (args.length === 0) {
+    console.log('No argument');
+  } else if (args.length === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+};
