ee111b45b90970385da44195a5aa71d1810573c7
michael <michaelesigbone@gmail.com>
2025-11-09 16:22:13 +0100

    New

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
 function argNum () {
   if (arguments.length === 0) {
     console.log('No Argument');
-  } else if (arguments.lengthh === 1) {
+  } else if (arguments.length === 1) {
     console.log('Argument found');
   } else {
     console.log('Arguments found');
