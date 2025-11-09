fc45c441ffc6d24551e4178c6f04cfb3ab1d4bd3
michael <michaelesigbone@gmail.com>
2025-11-09 14:12:52 +0100

    Edit name

---
+++ 1-multi_languages.js
@@ -0,0 +1,6 @@
+const firstSentence = 'C is fun';
+const secondSentence = 'Python is cool';
+const thirdSentence = 'JavaScript is amazing';
+console.log(firstSentence);
+console.log(secondSentence);
+console.log(thirdSentence);
