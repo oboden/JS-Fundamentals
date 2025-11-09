c5d57612d29ac6c2f1713a69c58ee57deb311eaa
michael <michaelesigbone@gmail.com>
2025-11-09 14:23:31 +0100

    Made correction

--- 2-arguments.js
+++ 2-arguments.js
@@ -4,7 +4,7 @@ const newFunc = function () {
 
 if (newFunc === undefined) {
   console.log('No argument')
-} else if (newFunc !== 1) {
+} else if (newFunc === 1) {
   console.log('Argument found')
 } else {
   console.log('Arguments found')
