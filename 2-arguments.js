93e470a350062316dc446b92ba7f10b7ed3cff5d
michael <michaelesigbone@gmail.com>
2025-11-09 15:09:42 +0100

    Try something new

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,11 @@
-const newFunc = function () {
-
+const newFunc = function (name) {
+  console.log(`New person ${name}`)
 }
-if (newFunc === undefined ) {
+console.log(newFunc('Michael'));
+
+if (newFunc()) {
   console.log('No argument')
-} else if (newFunc !== undefined) {
+} else if (newFunc('')) {
   console.log('Argument found')
 } else {
   console.log('Arguments found')
