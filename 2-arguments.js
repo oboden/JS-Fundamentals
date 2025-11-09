03bd4dedeba413b0b252f003807576a4f2371710
michael <michaelesigbone@gmail.com>
2025-11-09 14:34:53 +0100

    New changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -4,7 +4,7 @@ const newFunc = function (name) {
   if (newFunc === 0) {
   console.log('No argument')
 } else if (newFunc === 1) {
-  console.log('Argument found')
-} else {
   console.log('Arguments found')
+} else {
+  console.log('Argument found')
 }
