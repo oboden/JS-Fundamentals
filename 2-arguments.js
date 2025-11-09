507ba3e604728c91bf9a8bdc1628e5d70a46e2c4
michael <michaelesigbone@gmail.com>
2025-11-09 14:30:38 +0100

    Change position of code

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,11 @@
 const newFunc = function (name) {
-  console.log(newFunc())
-}
-
-if (newFunc === 0) {
+  
+  if (newFunc === 0) {
   console.log('No argument')
 } else if (newFunc === 1) {
   console.log('Argument found')
 } else {
   console.log('Arguments found')
+}
+
 }
