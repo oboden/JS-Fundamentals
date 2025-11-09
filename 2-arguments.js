55f3064a768e3b14f2009bdde6bdc96fe15119a0
michael <michaelesigbone@gmail.com>
2025-11-09 16:19:40 +0100

    Trying a new thing

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
 function argNum (arg1, arg2, arg3) {
-  if (argNum.length === 0) {
+  if (arguments.length === 0) {
     console.log('No Argument');
-  } else if (argNum.length === 1) {
+  } else if (arguments.lengthh === 1) {
     console.log('Argument found');
   } else {
     console.log('Arguments found');
